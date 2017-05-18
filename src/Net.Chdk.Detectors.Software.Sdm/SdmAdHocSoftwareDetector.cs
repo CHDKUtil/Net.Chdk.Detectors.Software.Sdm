3d3630963da0eb4153fe9a4b1f1fa0964646a713
Dmitry Shechtman <damnian@gmail.com>
2017-05-18 08:40:12 +0300

    Add SdmAdHocSoftwareDetector

---
+++ src/Net.Chdk.Detectors.Software.Sdm/SdmAdHocSoftwareDetector.cs
@@ -0,0 +1,28 @@
+﻿using Net.Chdk.Detectors.Software.Binary;
+using Net.Chdk.Providers.Software;
+using System;
+
+namespace Net.Chdk.Detectors.Software.Sdm
+{
+    sealed class SdmAdHocSoftwareDetector : ProductBinarySoftwareDetector
+    {
+        public SdmAdHocSoftwareDetector(ISourceProvider sourceProvider)
+            : base(sourceProvider)
+        {
+        }
+
+        public override string ProductName => "SDM";
+
+        protected override int StringCount => 2;
+
+        protected override string[] Strings => new[]
+        {
+            "SDM ver. ",
+        };
+
+        protected override Version GetProductVersion(string[] strings)
+        {
+            return GetVersion(strings[0]);
+        }
+    }
+}
